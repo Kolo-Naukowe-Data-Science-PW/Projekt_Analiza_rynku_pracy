a3b9009f3b9ce9eca0a242037e638d574fa5bc0b
rifti <michal.kurtys@gmail.com>
2016-04-28 23:44:23 +0200

    rewrite 1

--- process.py
+++ process.py
@@ -9,12 +9,15 @@ import pandas as pd
 import sqlite3
 import lxml.html
 import re
+import numpy as np
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import BernoulliNB
 
 
 
 
-
-dbpath = "oferty.db"
+dbpath = "oferty_lang.db"
 conn = sqlite3.connect(dbpath)
 o = pd.read_sql_query("SELECT nazwa,firma, miejsce,opis FROM oferty",conn)
 conn.close()
@@ -63,80 +66,66 @@ for html_content in html:
     
 
 #for offer in text_offers:
-o["text"] = text_offers    
+o["text"] = text_offers
+o = o.iloc[0:1000,]    
 
 
-
-    
-oferujemy = []
-r = re.compile(r"(Ofer.*\n+(\W*[\*-].*\n)(.+\n)*)",re.I | re.M)
-for i,t in enumerate(text_offers):
-    m = r.search(t)
-    if m:
-        #o[i].oferta = m.group(0)
-        oferujemy.append(m.group(0))
-    else:
-        #oferujemy.append('') #or...None?
-        oferujemy.append(None)
-        
-
-        
-o["oferujemy"] = oferujemy   
-
-
-print( "Number of offer rows: {0}".format(len(o)))
-print( "Number of oferujemy rows: {0}".format(
-    o["oferujemy"].isnull().sum()))
-
-
-other_lists = []        
-r_all_lists = re.compile(r"^(.*\n+(\W*[\*-].*\n)(.+\n)*)",re.I | re.M)
-for t in o["text"][o["oferujemy"].notnull()].values:
-    matches = r_all_lists.findall(t)
-    #print(matches)
+offer_lists = pd.DataFrame(columns=("offer_id", "list"), dtypes=)
+re_all_lists = re.compile(r"^(.*\n+(\W*[\*-].*\n)(.+\n)*)",re.I | re.M)
+for i, row in o.iterrows():
+    print(i)
+    print(type(i))
+    t = row["text"]
+    matches = re_all_lists.findall(t)
     for m in matches:
-        m = m[0]
-        #print(m)
-        if not r.match(m):
-            other_lists.append(m)
-        
-offer_list = [x for x in oferujemy if x is not None ]
-
-to_classify = []
-to_cls_id = []        
-for i,t in enumerate(o["text"].values):
-    if not o["oferujemy"][i]:
-        matches = r_all_lists.findall(t)
-        for m in matches:
-            m = m[0]
-            to_classify.append(m)
-            to_cls_id.append(i)
-
+        offer_lists.loc[ len(offer_lists) ] = [i,m[0]]
         
         
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import BernoulliNB
 
-inp = offer_list + other_lists
-out = [1]*len(offer_list) + [0]*len(other_lists)
+def bayes_one(list_table, reg, attr):
+    r = re.compile(reg)
+    good_parts_ids = []
+    bad_parts = []
+    good_parts = []
+    for i, row in list_table.iterrows():
+        if r.match(row["list"]):
+            good_parts_ids.append(i)
+            good_parts.append(row["list"])
+    
+    for gi in good_parts_ids:
+        #take bad parts
+        offer_id = 
+        bad = list_table[ list_table["offer_id"] == gi ]
+        print(bad)
+        bad = bad[ bad.index != gi ]
+        #print(bad)
+        if len(bad) > 0:
+            for i, row in bad.iterrows():
+                bad_parts.append(row["list"])
+            
+    #for g in good_parts:
+    #    print(g)
 
-count_vectorizer = CountVectorizer(binary=True)
-#counts = count_vectorizer.fit_transform(o['oferujemy'].dropna().values)
-counts = count_vectorizer.fit_transform(inp)
+    #for b in bad_parts:
+    #    print(b)
+            
+#    inp = good_parts + bad_parts
+#    out = [1]*len(good_parts) + [0]*len(bad_parts)
+#    
+#    count_vectorizer = CountVectorizer(binary=True)
+#    #counts = count_vectorizer.fit_transform(o['oferujemy'].dropna().values)
+#    counts = count_vectorizer.fit_transform(inp)
+#
+#    classifier = BernoulliNB()
+#    classifier.fit(counts, out)
+#    
+#    cls_counts = count_vectorizer.transform(list_table["list"])
+#    predictions = classifier.predict(cls_counts)
+#    list_table[attr] = predictions
+            
 
-classifier = BernoulliNB()
+bayes_one(offer_lists, r"(Ofer.*\n+(\W*[\*-].*\n)(.+\n)*)"  , "oferujemy" )
 
-classifier.fit(counts, out)
-
-cls_counts = count_vectorizer.transform(to_classify)
-
-predictions = classifier.predict(cls_counts)
-           
-
-for x in range(40):
-    print(to_cls_id[x])
-    print(predictions[x])
-    print(to_classify[x])
 
 
 
