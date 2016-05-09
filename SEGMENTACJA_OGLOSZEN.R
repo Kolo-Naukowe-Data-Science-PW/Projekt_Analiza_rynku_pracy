dc928b2a0e913c5b845a83535ba447a33da846a1
ryciakn <norbertryciak@gmail.com>
2016-05-09 19:03:22 +0200

    gotowa segmentacja ogloszen - bez klasyfikacji - plik SEGMENTACJA_OGLOSZEN.R

--- SEGMENTACJA_OGLOSZEN.R
+++ SEGMENTACJA_OGLOSZEN.R
@@ -2,10 +2,6 @@ library(XML)
 library(RSQLite)
 library(stringi)
 
-con = dbConnect(SQLite(),"F:/Norbert/studia/KoloNaukoweDS/Projekty/Analiza_rynku_pracy/oferty_lang.db")
-dbListFields(con,"oferty")
-x <- dbGetQuery(con,"select opis from oferty limit 10000")[[1]]
-
 convert_and_prepare <- function(x){
   
   x <- as(x,"character") # konwersja 
@@ -29,63 +25,45 @@ convert_and_prepare <- function(x){
   x
 }
 
-for(i in 1:10000){
- doc = htmlTreeParse(x[i], useInternalNodes = T,encoding = 'UTF8')
- 
- div <- unlist(lapply(getNodeSet(doc, "//div"), convert_and_prepare ))
- 
- lists <- unlist(stri_extract_all_regex(div[1],"(?<=(<ul[^>]{0,100}>))[\\w\\W]+?(?=(</ul>))",multiline=TRUE))
- 
- #wyciaganie naglowkow list - nie dziala
- lists_headres <- stri_trim_both(unlist(
-    stri_extract_all_regex(
-     div[1]
-      ,"(?<=(>))\\s*[^>]+?;?:?\\s*(?=((<[^>]+?>\\s*)*?<ul[^>]{0,100}>))",multiline=TRUE)
-  ))
-  lists_headres <- lists_headres[!is.na(lists_headres)]
-  lists_headres <- lists_headres[stri_length(lists_headres)>3] #dla bezpieczenstwa zeby jakis smiec nie wpadl
- 
- 
- div <- div[stri_count_fixed(div,"<div")==1]
- 
- #print(i)
- #print(lists_headres)
- if(!any(is.na(lists))){
-   stopifnot(length(lists_headres)==length(lists))
- }
- 
- end <- which(stri_detect_regex(div,"<ul>|<div id=\"clause\"|<div id=\"contact\"|class=\"clause\"|1997")) 
- # po divach, w ktorych znajduje sie cos takiego juz nigdy nic wartosciowego nie ma 
- if(length(end)>0){
-   div <- div[1:min(end)-1] #bierzemy wszystkie znaczniki poprzedzajace bez tych w ktorych juz nic nie ma 
- }
- div <- stri_replace_all_regex(div,"<ul>(.|\\s)+?</ul>","",multiline = TRUE)
- div <- stri_replace_all_regex(div,"<(.|\\s)+?>","")
- div <- stri_replace_all_regex(div,"\\W+"," ")
- div <- div[stri_length(div)>150]
- div <- div[!stri_detect_regex(div,"CV|aplikowania|aplikacji|aplikacjÄ™|motywacyjnego|Aplikuj|Aplikujpraca|UstawÄ…")]
- 
- #print(lists)
+
+f <- function(opis, min_length = 150) {
+  
+  opis <- stri_replace_all_regex(opis, "<li[^>]*?>", "<li> ")
+  doc <- htmlTreeParse(opis, useInternalNodes = TRUE, encoding = 'UTF-8')
+  root <- xmlRoot(doc)
+  
+  # pierwszy tag zawieraj¹cy tekst, który poprzedza jak¹œ niezagnie¿d¿on¹ listê
+  xpath <- "//ul[not(ancestor::ul)]/preceding::*[text()][1]"
+  
+  nodes <- getNodeSet(root, xpath)
+  nazwy <- sapply(nodes, xmlValue)
+  #removeNodes(nodes)
+  
+  nodes <- getNodeSet(root, "//ul[not(ancestor::ul)]")
+  listy <- sapply(nodes, xmlValue)
+  removeNodes(nodes)
+  
+  listy <- structure(listy, names = c(nazwy))
+  
+  
+  div <- unlist(lapply(getNodeSet(doc, "//div"), convert_and_prepare ))
+  div <- div[stri_count_fixed(div,"<div")==1] # bierzemy tylko "liscie" 
+  
+  end <- which(stri_detect_regex(div,"<div id=\"clause\"|<div id=\"contact\"|class=\"clause\"|1997")) 
+  # po divach, w ktorych znajduje sie cos takiego juz nigdy nic wartosciowego nie ma 
+  if(length(end)>0){
+    div <- div[1:min(end)-1] #bierzemy wszystkie znaczniki poprzedzajace bez tych w ktorych juz nic nie ma 
+  }
+  
+  div <- stri_replace_all_regex(div,"<(.|\\s)+?>","")
+  div <- stri_replace_all_regex(div,"\\W+"," ")
+  div <- div[stri_length(div)>min_length]
+  div <- div[!stri_detect_regex(div,"CV|aplikowania|aplikacji|aplikacjê|motywacyjnego|Aplikuj|Aplikujpraca|Ustaw¹")]
+  
+  
+  c(div, listy)
+  
+  
 }
 
 
-dbDisconnect(con)
-
-
-# smieci
-
-
-
-#if(is.na(lists)){ lists = character(0)}
-#lists <- unlist(lapply(getNodeSet(div[1], "//ul"), convert_and_prepare)) #konwersja do napisu
-
-
-#czy_sa_zagniezdzone <- stri_detect_fixed(lists,"<ul>")
-#if(any(czy_sa_zagniezdzone)){
-#  ind <- which(czy_sa_zagniezdzone)
-#  for(a in ind){
-#     lista_zagniezdzona <- unlist(stri_extract_all_regex(lists[a],"(?<=(<ul>))[\\w\\W]+?(?=(</ul>))",multiline=TRUE))
-#    lists[a] <- stri_replace_all_regex(lists[a],"<ul>[\\w\\W]+","",multiline = TRUE)
-#     lists[a] <- stri_paste(lists[a],lista_zagniezdzona)
-#   }
-# }
